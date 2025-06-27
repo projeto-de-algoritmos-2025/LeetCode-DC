ac0d8c1c22e5f1d1617dbd318a6dfa666cbd5c4a
ViictorHugoo <victorhugorodriguesguimaraes@gmail.com>
2025-06-27 17:58:17 -0300

    remove comentario 23

--- 23.py
+++ 23.py
@@ -1,12 +1,5 @@
 from typing import List, Optional
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
